a72243f3dc1f9fe6a25ee94764d1bb40ce497766
Chuck Monarch <cmonarch@watchsystems.com>
2021-07-14 14:31:28 -0500

    Added Offender class

---
+++ Domain/Offender.cs
@@ -0,0 +1,22 @@
+namespace Domain
+{
+    public class Offender
+    {
+         public string MKE { get; set; }
+         public string ORI { get; set; }
+         public string NIC { get; set; }
+         public string NAM { get; set; }
+         public string SEX { get; set; }
+         public string RAC { get; set; }
+         public string POB { get; set; }
+         public string DOB { get; set; }
+         public string ORD { get; set; }
+         public string ERD { get; set; }
+         public string SXP { get; set; }
+         public string HGT { get; set; }
+         public string WGT { get; set; }
+         public string EYE { get; set; }
+         public string HAI { get; set; }
+         public string FBI { get; set; }
+    }
+}
